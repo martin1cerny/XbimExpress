427afe8013534687419b4e9e6fa57be8f89766de
Martin Cerny <martin1cerny@gmail.com>
2015-10-23 16:43:10 +0200

    Created new project to generate most of the code for parse-time translation between different versions of IFC

---
+++ XbimTranslatorGenerator/Templates/CSProjectTemplatePartial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace XbimTranslatorGenerator.Templates
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class CSProjectTemplate
+    {
+        public string Name { get; private set; }
+
+        public string Id { get; private set; }
+
+        public CSProjectTemplate(string projectName)
+        {
+            Name = projectName;
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ XbimTranslatorGenerator/Templates/ITranslatorTemplatePartial.cs
@@ -0,0 +1,17 @@
+﻿namespace XbimTranslatorGenerator.Templates
+{
+    // ReSharper disable once InconsistentNaming
+    public partial class ITranslatorTemplate:ICodeTemplate
+    {
+
+        public string Name
+        {
+            get { return "ITranslator"; }
+        }
+
+        public string Namespace
+        {
+            get { return Settings.Namespace; }
+        }
+    }
+}
