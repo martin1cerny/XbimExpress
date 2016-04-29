82df88fbf7a31bc038e4cbe9ed906feb37c40d0e
Martin Cerny <martin1cerny@gmail.com>
2016-04-29 16:55:17 +0200

    Deleted usings

--- Xbim.CodeGeneration/Templates/EntityInterfaceTemplatePartial.cs
+++ Xbim.CodeGeneration/Templates/EntityInterfaceTemplatePartial.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xbim.CodeGeneration.Helpers;
 using Xbim.CodeGeneration.Settings;
 using Xbim.ExpressParser.SDAI;
