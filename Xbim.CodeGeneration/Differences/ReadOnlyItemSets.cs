12a9e781a5be44cddde79393fd64258c23077c7d
Martin Cerny <martin1cerny@gmail.com>
2016-06-14 12:25:02 +0200

    Editable ItemSets on interfaces almost finished.

---
+++ Xbim.CodeGeneration/Differences/ReadOnlyItemSets.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xbim.CodeGeneration.Differences
+{
+    internal class ReadOnlyItemSets: IEnumerable<ItemSetAttribute>
+    {
+        private static readonly List<ItemSetAttribute> Attributes = new List<ItemSetAttribute>(); 
+
+        static ReadOnlyItemSets()
+        {
+            var data = Properties.Resources.ReadonlyItemSets;
+            using (var reader = new StringReader(data))
+            {
+                var line = reader.ReadLine();
+                while (!string.IsNullOrWhiteSpace(line))
+                {
+                    var parts = line.Split(',').Select(p => p.Trim()).ToArray();
+                    Attributes.Add(new ItemSetAttribute {Class = parts[0], Attribute = parts[1]});
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        public IEnumerator<ItemSetAttribute> GetEnumerator()
+        {
+            return Attributes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    internal class ItemSetAttribute
+    {
+        public string Class { get; set; }
+        public string Attribute { get; set; }
+    }
+}
